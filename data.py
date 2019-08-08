6762aece3f5ecdea93cc6116fdb171211e9f35b9
kamil <kammmilkos@gmail.com>
2019-08-08 17:31:06 +0200

    Add data.py file mockup

---
+++ data.py
@@ -0,0 +1,35 @@
+data = {
+    'series': 'Onii-chan_Dakedo_Ai_Sae_Areba_Kankeinai_yo_ne_Specials',
+    'episodes': [
+        {
+            'number': 1,
+            'name': '1.mp4',
+            'ground_truth': [(780, 1158), (2239, 2671), (2762, 2944), (3132, 3316), (3427, 3538), (3544, 3643), (3649, 3749)]
+        },
+        {
+            'number': 2,
+            'name': '2.mp4',
+            'ground_truth': []
+        },
+        {
+            'number': 3,
+            'name': '3.mp4',
+            'ground_truth': []
+        },
+        {
+            'number': 4,
+            'name': '4.mp4',
+            'ground_truth': []
+        },
+        {
+            'number': 5,
+            'name': '5.mp4',
+            'ground_truth': []
+        },
+        {
+            'number': 6,
+            'name': '6.mp4',
+            'ground_truth': []
+        },
+    ]
+}
